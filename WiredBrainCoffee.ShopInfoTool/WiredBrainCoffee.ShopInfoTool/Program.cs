5f421e784262952096ecee0eaa6b90292d024219
Kim Beckers <kbeckers@descartes.com>
2022-05-16 21:20:10 +0200

    Add project

---
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WiredBrainCoffee.ShopInfoTool {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+        }
+    }
+}
